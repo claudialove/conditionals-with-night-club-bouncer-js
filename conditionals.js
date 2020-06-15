99b7c9725c85e34a239c3914f2ceb4b5b591c24f
Claudia Petty <claudialove32@gmail.com>
2020-06-15 17:34:19 -0400

    inital and likely only push

---
+++ conditionals.js
@@ -0,0 +1,8 @@
+var age = prompt ("How old are you?")
+if (age < 18) {
+    alert ("You cannot enter the venue!!");
+} else if (age >= 18 && age <= 21) {
+    alert ("You may enter, but YOU MAY NOT drink!!");
+} else {
+    alert("Come on in and have a drink ole'friend!!")
+};
